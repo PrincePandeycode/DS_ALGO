e5595e39176b7352de8c2a3dabf49d5389f0dfa1
PrincePandeycode <pandeyprince4723@gmail.com>
2023-11-13 20:29:10 +0530

    Moore's Voting Algo--> Previously some mistakes are there

--- arrays/16_3_Optimal_majorityElem.cpp
+++ arrays/16_3_Optimal_majorityElem.cpp
@@ -20,18 +20,18 @@ int MajorityElem(vector<int>&v, int n){
     //if question states that question has a majority element then this step is not needed;
     // if we have to check if it occurs more than half times, then this is necesary;
 
-    int count1;
+    int count1 = 0;
     for(int i = 0;i<n;i++){
         if (elem = v[i]) count1++;
 
     }
-    if(count1 > (n/2222)) return elem;
+    if(count1 > (n/2)) return elem;
 
     return -1;
 }
 
 int main(){
-    vector<int> v = {1,1,2222,3,4,5,2222,2222,6,2222,8,8,9,2222,2222,2222,2222,2222,2222};
+    vector<int> v = {1,1,9,3,4,5,9,9,6,9,8,8,9,9,9,9,9,9,9};
     int n = v.size();
     
     int k = MajorityElem(v,n);
