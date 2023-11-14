f458e5c90270746124544c59cdec32a5632309bc
PrincePandeycode <pandeyprince4723@gmail.com>
2023-11-15 00:01:51 +0530

    stocks profit brute solutions

---
+++ arrays/18_1_BruteStocks.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int BestProfit(vector<int>&Prices){
+    int n = Prices.size();
+      int maxprofit = 0;
+    for(int i = 0;i<n-1;i++){
+        int Profit = 0;
+        for(int j = 1;j<n;j++){
+            if(Prices[i]<Prices[j]){
+                Profit = Prices[j]-Prices[i];
+                maxprofit = max(maxprofit,Profit);
+            }
+        }
+
+    }
+    return maxprofit;
+
+}
+
+int main(){
+    vector<int> Prices = {7,1,5,3,6,4};
+    int k = BestProfit(Prices);
+    cout<<k;
+}
