3cce80593a351108582894cda6313de7f2cbd1d6
PrincePandeycode <pandeyprince4723@gmail.com>
2023-11-21 16:46:55 +0530

    total number of subarrays with sum == k

---
+++ arrays/26_1_brute_maximumNoOFSubarrays.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int maxSubarrays(vector<int>&v,int k){
+    int n = v.size();
+    int count = 0;
+       
+    for(int i = 0;i<n;i++){
+       
+           
+        for(int j = i;j<n;j++){
+
+
+               int sum = 0;
+            for(int k = i;k<=j;k++){
+            sum = sum+v[k];
+            // sum += v[j];
+            }
+            if(sum == k){
+                count++;
+            }
+        }
+    }
+    return count;
+}
+int main(){
+    vector<int> v= {1,2,3,-3,1,1,1,4,2,-3};
+    int k = 3;
+    int ans = maxSubarrays(v,k);
+    cout<<ans;
+}
