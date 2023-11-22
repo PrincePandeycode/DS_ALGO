b06ac6c1b46968b3ef16f49b94423c179e498133
PrincePandeycode <pandeyprince4723@gmail.com>
2023-11-23 00:22:04 +0530

    corrected optimal approach of finding element using row number and colnumber pascal triangle

--- arrays/ArrayHard/27_2optimal_FIndElementOfPascalUsingRowClo.cpp
+++ arrays/ArrayHard/27_2optimal_FIndElementOfPascalUsingRowClo.cpp
@@ -2,27 +2,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int nCr(int n, int r) {
+int nCr(int nn, int rr) {
+    int n = nn-1;
+    int r = rr-1;
     long long res = 1;
 
-    // calculating nCr:
-    for (int i = 0; i < r; i++) {
-        res = res * (n - i);
-        res = res / (i + 1);
-    }
+  for(int i = 0;i<r;i++){
+     res *=(n-i);
+    res /=(i+1);
+  }
     return res;
 }
 
-int pascalTriangle(int r, int c) {
-    int element = nCr(r - 1, c - 1);
-    return element;
-}
+
 
 int main()
 {
     int r = 5; // row number
     int c = 3; // col number
-    int element = pascalTriangle(r, c);
+    int element = nCr(r, c);
     cout << "The element at position (r,c) is: "
             << element << "n";
     return 0;
