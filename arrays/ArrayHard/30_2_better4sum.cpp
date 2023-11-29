9a390e708a8271d83fa119b7ac5e16dde45a156c
PrincePandeycode <pandeyprince4723@gmail.com>
2023-11-29 22:41:30 +0530

    4 sum problem better approach

--- arrays/ArrayHard/30_2_better4sum.cpp
+++ arrays/ArrayHard/30_2_better4sum.cpp
@@ -11,20 +11,20 @@ vector<vector<int>> fourSum(vector<int>& nums, int target) {
     //checking all possible quadruplets:
     for (int i = 0; i < n; i++) {
         for (int j = i + 1; j < n; j++) {
+            set<long long> hashset;
             for (int k = j + 1; k < n; k++) {
-                for (int l = k + 1; l < n; l++) {
-                    // taking bigger data type
-                    // to avoid integer overflow:
-                    long long sum = nums[i] + nums[j];
-                    sum += nums[k];
-                    sum += nums[l];
-
-                    if (sum == target) {
-                        vector<int> temp = {nums[i], nums[j], nums[k], nums[l]};
-                        sort(temp.begin(), temp.end());
-                        st.insert(temp);
-                    }
+                // taking bigger data type
+                // to avoid integer overflow:
+                long long sum = nums[i] + nums[j];
+                sum += nums[k];
+                long long fourth = target - sum;
+                if (hashset.find(fourth) != hashset.end()) {
+                    vector<int> temp = {nums[i], nums[j], nums[k], (int)(fourth)};
+                    sort(temp.begin(), temp.end());
+                    st.insert(temp);
                 }
+                // put the kth element into the hashset:
+                hashset.insert(nums[k]);
             }
         }
     }
