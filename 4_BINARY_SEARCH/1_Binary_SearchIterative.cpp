52fa98a4bb012ba64ae450cbe7dc38b1fbbff8c4
PrincePandeycode <pandeyprince4723@gmail.com>
2023-12-29 23:07:00 +0530

    binary search itertive way repaeted

--- 4_BINARY_SEARCH/1_Binary_SearchIterative.cpp
+++ 4_BINARY_SEARCH/1_Binary_SearchIterative.cpp
@@ -1,29 +1,22 @@
-
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-int binarySearch(vector<int>& nums, int target) {
-    int n = nums.size(); //size of the array
-    int low = 0, high = n - 1;
-
-    // Perform the steps:
-    while (low <= high) {
-        int mid = (low + high) / 2;
-        if (nums[mid] == target) return mid;
-        else if (target > nums[mid]) low = mid + 1;
-        else high = mid - 1;
+int BnarySearch(vector<int>&arr, int target){
+    int n = arr.size();
+    int low = 0;
+    int high = n-1;
+    while(low<=high){
+        int mid = (low+high)/2;
+        if(arr[mid] == target) return mid;
+        else if(arr[mid]>target) high = mid-1;
+        else low = mid+1;
     }
     return -1;
 }
+int main(){
+     vector<int> a = {3, 4, 6, 7, 9, 12, 16, 17};
+     int target = 6;
+     int ans = BnarySearch(a,target);
 
-int main()
-{
-    vector<int> a = {3, 4, 6, 7, 9, 12, 16, 17};
-    int target = 6;
-    int ind = binarySearch(a, target);
-    if (ind == -1) cout << "The target is not present." << endl;
-    else cout << "The target is at index: "
-                  << ind << endl;
-    return 0;
+     cout<<ans;
 }
-
