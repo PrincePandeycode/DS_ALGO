db214991e01121c77a9adf1724ddb53a6fdf5741
PrincePandeycode <pandeyprince4723@gmail.com>
2023-12-31 21:28:58 +0530

    upper bound c++ predefined function

--- 4_BINARY_SEARCH/4_2_upperBound.cpp
+++ 4_BINARY_SEARCH/4_2_upperBound.cpp
@@ -21,7 +21,8 @@ int main()
 {
     vector<int> arr = {3, 5, 8, 9, 15, 19};
     int n = 6, x = 9;
-    int ind = upperBound(arr, x, n);
+    // int ind = upperBound(arr, x, n);
+    int ind = upper_bound(arr.begin(),arr.end(),x)-arr.begin();
     cout << "The upper bound is the index: " << ind << "\n";
     return 0;
 }
