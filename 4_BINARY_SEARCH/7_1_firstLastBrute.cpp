8d262052216bc53f09bfe92e82cee41d67f132e3
PrincePandeycode <pandeyprince4723@gmail.com>
2024-01-01 21:46:08 +0530

    find first ans last occurance brute force lon(N)

--- 4_BINARY_SEARCH/7_1_firstLastBrute.cpp
+++ 4_BINARY_SEARCH/7_1_firstLastBrute.cpp
@@ -3,14 +3,15 @@
 using namespace std;
 
 pair<int, int> firstAndLastPosition(vector<int>& arr, int n, int k) {
-    int first = -1, last = -1;
-    for (int i = 0; i < n; i++) {
-        if (arr[i] == k) {
-            if (first == -1) first = i;
-            last = i;
+    int firstel = -1;
+    int lastel = -1;
+    for(int i = 0;i<n;i++){
+        if(arr[i] == n){
+            if(firstel == -1) firstel = i;
+            lastel = i;
         }
     }
-    return {first, last};
+    return {firstel,lastel};
 }
 
 
