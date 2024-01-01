3031e61bd0ac79de1d301878e2a40b51219b6736
PrincePandeycode <pandeyprince4723@gmail.com>
2024-01-01 22:55:14 +0530

    find first ans last occurance solution using upper and lower bound

--- 4_BINARY_SEARCH/7_2_firstlastbetter.cpp
+++ 4_BINARY_SEARCH/7_2_firstlastbetter.cpp
@@ -1,53 +1,49 @@
+#include<bits/stdc++.h>
 
-#include <bits/stdc++.h>
 using namespace std;
 
-int upperBound(vector<int> &arr, int n, int x) {
-    int low = 0, high = n - 1;
-    int ans = n;
 
-    while (low <= high) {
-        int mid = (low + high) / 2;
-        // maybe an answer
-        if (arr[mid] > x) {
+int upperB(vector<int>&arr,int n,int k){
+    int low = 0;
+    int high = n-1;
+    int ans = n;
+    while(low<= high){
+        int mid = (low+high)/2;
+        if(arr[mid]>k){
             ans = mid;
-            //look for smaller index on the left
-            high = mid - 1;
+            high = mid-1;
         }
-        else {
-            low = mid + 1; // look on the right
-        }
+        else low = mid+1;
     }
     return ans;
 }
 
-int lowerBound(vector<int> &arr, int n, int x) {
-    int low = 0, high = n - 1;
+int lowerB(vector<int>&arr,int n,int k){
+
+    int low = 0;int high = n-1;
     int ans = n;
 
-    while (low <= high) {
-        int mid = (low + high) / 2;
-        // maybe an answer
-        if (arr[mid] >= x) {
+    while(low<=high){
+        int mid = (low+high)/2;
+        if(arr[mid]>=k){
             ans = mid;
-            //look for smaller index on the left
-            high = mid - 1;
+            high = mid-1;
         }
-        else {
-            low = mid + 1; // look on the right
-        }
+        else low= mid+1;
     }
     return ans;
+    
 }
 
+pair<int,int> firstAndLastPosition(vector<int>&arr,int n,int k){
+    int ind1 = upperB(arr,n,k);
+    int ind2 = lowerB(arr,n,k);
 
-pair<int, int> firstAndLastPosition(vector<int>& arr, int n, int k) {
-    int lb = lowerBound(arr, n, k);
-    if (lb == n || arr[lb] != k) return { -1, -1};
-    int ub = upperBound(arr, n, k);
-    return {lb, ub - 1};
+    if(ind2 == n|| arr[ind2]!=k) return{-1,-1};
+    ind1 = ind1-1;
+
+  return {ind2,ind1};
 }
-
 
 int main()
 {
