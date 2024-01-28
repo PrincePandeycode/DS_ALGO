91a390c21c21ae6d0716c2327b6550ad676a086a
PrincePandeycode <pandeyprince4723@gmail.com>
2024-01-29 00:16:37 +0530

    search in rotatd sorted array better solution

--- 4_BINARY_SEARCH/9_2OptimalSeaechrotated.cpp
+++ 4_BINARY_SEARCH/9_2OptimalSeaechrotated.cpp
@@ -2,35 +2,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int search(vector<int>& arr, int n, int k) {
-    int low = 0, high = n - 1;
-    while (low <= high) {
-        int mid = (low + high) / 2;
+int search(vector<int>& arr, int n, int target) {
+    int low = 0,high = n-1;
 
-        //if mid points the target
-        if (arr[mid] == k) return mid;
+    while(low<=high){
+        int mid = (low+high)/2;
 
-        //if left part is sorted:
-        if (arr[low] <= arr[mid]) {
-            if (arr[low] <= k && k <= arr[mid]) {
-                //element exists:
-                high = mid - 1;
+        if(arr[mid] ==target) return mid;
+
+        if(arr[low]<=arr[mid]){
+
+            if(arr[low]<=target && arr[mid]>=target){
+                high = mid-1;
             }
-            else {
-                //element does not exist:
-                low = mid + 1;
+            else{
+                low = mid+1;
             }
+
         }
-        else { //if right part is sorted:
-            if (arr[mid] <= k && k <= arr[high]) {
-                //element exists:
-                low = mid + 1;
+        else{
+
+            if(arr[mid]<=target && arr[high]>= target){
+                low = mid+1;
+
             }
-            else {
-                //element does not exist:
-                high = mid - 1;
+            else{
+                high = mid-1;
             }
+
         }
+        
     }
     return -1;
 }
@@ -38,7 +39,7 @@ int search(vector<int>& arr, int n, int k) {
 int main()
 {
     vector<int> arr = {7, 8, 9, 1, 2, 3, 4, 5, 6};
-    int n = 9, k = 1;
+    int n = 9, k = 6;
     int ans = search(arr, n, k);
     if (ans == -1)
         cout << "Target is not present.\n";
