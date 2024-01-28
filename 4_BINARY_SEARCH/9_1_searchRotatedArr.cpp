c1f369fb7f368c28fa68e3856a43dfd9ac955309
PrincePandeycode <pandeyprince4723@gmail.com>
2024-01-28 23:35:06 +0530

    find in a rotated sorted array brute solution

--- 4_BINARY_SEARCH/9_1_searchRotatedArr.cpp
+++ 4_BINARY_SEARCH/9_1_searchRotatedArr.cpp
@@ -1,23 +1,17 @@
-
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-int search(vector<int>& arr, int n, int k) {
-    for (int i = 0; i < n; i++) {
-        if (arr[i] == k)
-            return i;
+int search(vector<int>&a , int n , int k ){
+    for(int i = 0;i<n;i++){
+        if(a[i] == k) return i;
     }
     return -1;
 }
 
-int main()
-{
-    vector<int> arr = {7, 8, 9, 1, 2, 3, 4, 5, 6};
-    int n = 9, k = 1;
-    int ans = search(arr, n, k);
-    if (ans == -1)
-        cout << "Target is not present.\n";
-    else
-        cout << "The index is: " << ans << "\n";
-    return 0;
+int main(){
+    vector<int> a = {7,8,9,1,2,3,4,5,6};
+    int n = a.size();
+    int target = 2;
+    int ans = search(a,n,target);
+    cout<<ans;
 }
