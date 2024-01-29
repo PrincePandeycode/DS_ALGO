2bef62b0778195c8d951d94f2837202d898c66c0
PrincePandeycode <pandeyprince4723@gmail.com>
2024-01-29 14:16:49 +0530

    Optimal solution Find ELement in an rotated sorted array

--- 4_BINARY_SEARCH/10_2_optimalRotatedS2.cpp
+++ 4_BINARY_SEARCH/10_2_optimalRotatedS2.cpp
@@ -3,41 +3,31 @@
 using namespace std;
 
 bool searchInARotatedSortedArrayII(vector<int>&arr, int k) {
-    int n = arr.size(); // size of the array.
-    int low = 0, high = n - 1;
-    while (low <= high) {
-        int mid = (low + high) / 2;
+    int n = arr.size();
+    int low = 0;
+    int high = n-1;
 
-        //if mid points the target
-        if (arr[mid] == k) return true;
-
-        //Edge case:
-        if (arr[low] == arr[mid] && arr[mid] == arr[high]) {
-            low = low + 1;
-            high = high - 1;
+    while(low<=high){
+        int mid = (low+high)/2;
+        if(arr[mid] == k) return true;
+        if(arr[low] == arr[mid] && arr[mid] == arr[high]){
+            low++;
+            high--;
             continue;
         }
 
-        //if left part is sorted:
-        if (arr[low] <= arr[mid]) {
-            if (arr[low] <= k && k <= arr[mid]) {
-                //element exists:
-                high = mid - 1;
+        if(arr[low]<=arr[mid]){
+            if(arr[low]<=k && arr[mid]>= k){
+                high = mid-1;
             }
-            else {
-                //element does not exist:
-                low = mid + 1;
-            }
+            else low = mid+1;
         }
-        else { //if right part is sorted:
-            if (arr[mid] <= k && k <= arr[high]) {
-                //element exists:
-                low = mid + 1;
+        else{
+            if(arr[mid]<= k && arr[high]>=k){
+                low = mid+1;
             }
-            else {
-                //element does not exist:
-                high = mid - 1;
-            }
+            else high = mid-1;
+
         }
     }
     return false;
