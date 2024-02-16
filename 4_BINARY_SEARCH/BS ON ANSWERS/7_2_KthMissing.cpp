25969d956d04b945b779751b12671ac55ddcd9ba
PrincePandeycode <pandeyprince4723@gmail.com>
2024-02-16 20:39:39 +0530

    kth missing number optimal solution

---
+++ 4_BINARY_SEARCH/BS ON ANSWERS/7_2_KthMissing.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int kth(vector<int>& arr, int k){
+   int low = 0;
+   int n = arr.size();
+   int high = n-1;
+   while(low <=high){
+    int mid = (low+high)/2;
+    int missing = arr[mid]-(mid+1);
+
+    if(missing<k)low = mid+1;
+    else high = mid-1;
+   }
+   return low+k;
+}
+int main(){
+    vector<int> arr = {2,3,4,7,11};
+    int k = 5;
+
+    int ans = kth(arr,k);
+    cout<<ans;
+    return 0;
+}
