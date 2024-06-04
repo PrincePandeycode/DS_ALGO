421ad23e0a9966c1ef78b387a95e9e8d86a76016
PrincePandeycode <pandeyprince4723@gmail.com>
2024-06-04 16:41:15 +0530

    defanging an ip address

---
+++ strings(from SwherElse)/5_defangingIP.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+  string defangIPaddr(string address) {
+        int index = 0;
+        string ans;
+
+        while(index<address.size()){
+            if(address[index] == '.'){
+                ans = ans+"[.]";
+            }
+            else ans += address[index];
+            index++;
+        }
+        return ans;
+    }
+
+int main(){
+
+    string address = "1.1.1.1";
+
+    string ans = defangIPaddr(address);
+
+    cout<<ans;
+
+
+}
